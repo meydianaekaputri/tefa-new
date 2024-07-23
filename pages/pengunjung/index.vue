ba7cbb7ebc14584e0cd4f0d5bcde66c85123227e
meyyyy_art <142365545+meydianaekaputri@users.noreply.github.com>
2024-07-23 13:26:16 +0700

    Update index.vue

--- pages/pengunjung/index.vue
+++ pages/pengunjung/index.vue
@@ -21,8 +21,8 @@
                             <td>WAKTU</td>
                             <td>KEPERLUAN</td>
                         </tr>
-                        <tr v-for="(visitor,id) in visitors" :key="i">
-                            <td>{{ i+1 }}.</td>
+                        <tr v-for="(visitor,i) in visitors" :key="i">
+                            <td>{{ i+1 }}</td>
                             <td>{{ visitor.nama }}</td>
                             <td>{{ visitor.keanggotaan.nama }}</td>
                             <td>{{ visitor.tanggal }},{{ visitor.waktu}}</td>
